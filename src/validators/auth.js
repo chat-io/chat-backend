ca8b5896c2cb65484a53211cb2315b4cc4cec88e
Seong-Jin Kim <sys.ryan0902@gmail.com>
2021-12-22 19:22:42 +0900

    feat: auth validation 구현
    - src/validators/auth.js

---
+++ src/validators/auth.js
@@ -0,0 +1,24 @@
+const validator = require("validator");
+
+const authValidation = (user) => {
+  if (
+    validator.isEmpty(user.firstName) ||
+    validator.isEmpty(user.lastName) ||
+    validator.isEmpty(user.email) ||
+    validator.isEmpty(user.password)
+  ) {
+    return false;
+  }
+
+  if (!validator.isEmail(user.email)) {
+    return false;
+  }
+
+  if (!isByteLength(user.password, { min: 5 })) {
+    return false;
+  }
+
+  return true;
+};
+
+module.exports = { authValidation };
