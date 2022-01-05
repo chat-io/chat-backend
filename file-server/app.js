a43ec7ffa83f904ba081f74b6d4aec89b41e8184
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-01-06 04:35:37 +0900

    rafactor: file-server

--- file-server/app.js
+++ file-server/app.js
@@ -1,8 +1,9 @@
 const express = require("express");
 require("dotenv").config();
-const multer = require("multer");
+
 const path = require("path");
-const fs = require("fs");
+
+const avatarRoutes = require("./routes/avatar");
 const port = 3002;
 
 const app = express();
@@ -20,35 +21,7 @@ app.use((req, res, next) => {
   next();
 });
 
-const fileStorage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "avatar");
-  },
-  filename: (req, file, cb) => {
-    cb(null, `${req.params.userId}.png`);
-  },
-});
-const upload = multer({ storage: fileStorage });
-
-app.get("/", (req, res) => {
-  res.send("HELLO WORLD!");
-});
-
-app.put("/avatar/:userId", upload.single("avatar"), async (req, res) => {
-  res.send("file uploaded");
-});
-
-app.post("/avatar/:userId", async (req, res) => {
-  const avatarDirPath = path.join(__dirname, "avatar");
-  fs.copyFile(
-    path.join(avatarDirPath, "avatar.png"),
-    path.join(avatarDirPath, `${req.params.userId}.png`),
-    (err) => {
-      if (err) throw err;
-      res.status(200).send();
-    }
-  );
-});
+app.use("/avatar", avatarRoutes);
 
 app.listen(port, () => {
   console.log(`File server is running at http://localhost:${port}`);
